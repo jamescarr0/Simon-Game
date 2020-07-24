fb3bfc3659fa898907ec7750c1e6233b74a1e8a3
James Carr <jamescarr0@hotmail.com>
2020-07-24 13:32:29 +0100

    sound and visual animation feedback added.

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,15 +1,57 @@
+let level = 0
 let buttonColors = ["red", "blue", "green", "yellow"]
 let gamePattern = []
+let userPattern = []
+
+function btnFeedbackAnimation(btnColor) {
+    $("." + btnColor).addClass("flash")
+    setTimeout(function () {
+        $("." + btnColor).removeClass("flash")
+    }, 250)
+}
+
+function playSound(color) {
+    switch (color) {
+        case "red":
+            let red = new Audio("/sounds/red.mp3")
+            red.play()
+            break;
+        case "blue":
+            let blue = new Audio("/sounds/blue.mp3")
+            blue.play()
+            break;
+        case "green":
+            let green = new Audio("/sounds/green.mp3")
+            green.play()
+            break;
+        case "yellow":
+            let yellow = new Audio("/sounds/yellow.mp3")
+            yellow.play()
+            break;
+        default:
+            break;
+    }
+}
 
 function nextSequence() {
-    let randInt = Math.floor(Math.random()*4)
+    let randInt = Math.floor(Math.random() * 4)
     let color = buttonColors[randInt]
-
     gamePattern.push(color)
-
-    $("."+color).addClass("flash")
-    setTimeout(function() {
-        $("."+color).removeClass("flash")
-    }, 250)
-
+    btnFeedbackAnimation(color)
+    playSound(color)
 }
+
+$(".btn").on("click", function (e) {
+    /* 
+        Get the id of the button clicked event 
+        Remove the # sign from the beginning of the string.
+    */
+
+    let buttonClicked = e.target.id.slice(1,e.target.id.length)
+    userPattern.push(buttonClicked)
+    btnFeedbackAnimation(buttonClicked)
+    playSound(buttonClicked)
+})
+
+
+
