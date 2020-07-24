b0c337d073f51a98c2e0db57f3b28a0cf069cc38
James Carr <jamescarr0@hotmail.com>
2020-07-24 12:18:43 +0100

    Changes to css button classes

---
+++ js/gameLogic.js
@@ -0,0 +1,9 @@
+let buttonColors = ["red", "blue", "green", "yellow"]
+let gamePattern = []
+
+function nextSequence() {
+    gamePattern.push(buttonColors[Math.floor((Math.random()*4))])
+    console.log(gamePattern)
+
+    $()
+}
