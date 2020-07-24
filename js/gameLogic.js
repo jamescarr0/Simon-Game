d593a08bb1893a30cc71739e09f29cdd08aea426
James Carr <jamescarr0@hotmail.com>
2020-07-24 12:48:45 +0100

    intial js - random color and animated flash

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -2,8 +2,14 @@ let buttonColors = ["red", "blue", "green", "yellow"]
 let gamePattern = []
 
 function nextSequence() {
-    gamePattern.push(buttonColors[Math.floor((Math.random()*4))])
-    console.log(gamePattern)
+    let randInt = Math.floor(Math.random()*4)
+    let color = buttonColors[randInt]
 
-    $()
+    gamePattern.push(color)
+
+    $("."+color).addClass("flash")
+    setTimeout(function() {
+        $("."+color).removeClass("flash")
+    }, 250)
+
 }
